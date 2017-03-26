41380a322ad015dd984decc3b06e737b071ae1ad
Bryant Budiman <bbbudima@usc.edu>
2017-03-26 15:23:40 -0700

    ading slider

--- project2/resources/views/home.blade.php
+++ project2/resources/views/home.blade.php
@@ -15,6 +15,7 @@
 			margin-left: auto;
 			margin-bottom: auto;
 			text-align: center;
+			font-family: serif;
 		}
 
 		h1 {
@@ -41,7 +42,7 @@
 			color: black;
 			background-color: white;
 			font-size: 14px;
-			width: 18%;
+			width: 20%;
 			margin-top: 0%;
 			text-align: center;
 		}
@@ -49,23 +50,61 @@
 		.form {
 			margin-right: auto;
 			margin-left: auto;
-			font-family: serif;
 		}
+
+		$sliderWidth: 50% !default;
+		$sliderHandleDefaultColor: white !default;
+		$sliderHandlerHoverColor: black !default; 
+		$sliderHandlerSize: 10px !default;
+		$sliderTrackColor: #d7dcdf !default;
+		$sliderTrackHeight: 5px !default;
+		$sliderLabelColor: white !default;
+		$sliderLabelWidth: 16px !default;
+
+		.adjustSliderInfo {
+			color: white;
+			font-size: 14px;
+			width: 58%;
+		}
+
+		.slider {
+			margin: 38px 0 0 0%;
+			width: $sliderWidth;
+
+		}
+
+	
+
+
 	</style>
 
 </head>
 
 <body>
-	<div id="outerlayer">
-		<h1> Welcome to ACM and IEEE word cloud generator! <br> </h1>
+	<h1> Welcome to ACM and IEEE word cloud generator! <br> </h1>
 
-		{!! Form::open([]) !!}
+	{!! Form::open([]) !!}
 
-			{!! Form::text('textfield', null, ['class'=>'form', 'placeholder'=>"Type in a researcher's surname"]) !!}
-			{!! Form::submit('Generate word cloud',['class'=>'form']) !!}
+		{!! Form::text('textfield', null, ['class'=>'form', 'placeholder'=>"Type in a researcher's surname"]) !!}
+		{!! Form::submit('Generate word cloud',['id'=>'submitButtom', 'class'=>'form']) !!}
 
-		{!! Form::close() !!}
-	</div>
+		<span class="adjustSliderInfo"> <br> Set how many papers to include in the word cloud <br> </span>
+		
+		<div class="slider">
+			<input class="sliderTrack" type="range" min="0" max="50" value="10" step="1" onchange="showSliderValue(this.value)"/>
+			<span class="sliderLabel"> <br> 10 </span>
+		<
+
+
+	{!! Form::close() !!}
+
+	<script type="text/javascript">
+
+		function showSliderValue(newValue) {
+			document.getElementById("sliderValue").innerHTML = newValue;
+		}
+
+	</script>
 </body>
 
 </html>
