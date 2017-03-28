b7d4101cd689d6f6cddf0c60953cd8ff7ecc1ac4
alex man <alexman@usc.edu>
2017-03-28 15:39:19 -0700

    added functiont hat searches for IEEE term

--- project2/app/Http/Controllers/PagesController.php
+++ project2/app/Http/Controllers/PagesController.php
@@ -13,9 +13,22 @@ class PagesController extends Controller {
         return view('welcome');
     }
 
+    public function getSearchTerm($search_term)  {
+        $term = trim($search_term);
+        $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?querytext=$term";
+        $xml = simplexml_load_file($url, 'SimpleXMLElement', 
+            LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $array = json_decode($json, TRUE);
+
+        return view('cloud')->with('json_object', $array);
+
+    }
+
     public function goToCloudPage($researcher_name)
     {
-        return view('cloud')->with('researcher_name', $researcher_name);
+        return view('cloud')->with('researcher_name', 
+            $researcher_name);
     }
 
     public function postResearcherNameToCloudPage()
--- project2/routes/web.php
+++ project2/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function() {
 	return view('home');
 });
 
+// Route::get('/', 'ViewController@getStartView');
+
 Route::get('/cloud', function() {
 	return view('cloud');
 });
@@ -30,3 +32,4 @@ Route::get('/abstract', function() {
 Route::post('form', array('as' => 'form', 'uses'=>'PagesController@postResearcherNameToCloudPage'));
 
 Route::get('cloud/{researcher_name}/', array('as' => 'cloud', 'uses' => 'PagesController@goToCloudPage'));
+// Route::get('cloud/{researcher_name}/', array('as' => 'cloud', 'uses' => 'PagesController@getSearchTerm'));
