e01ba0a24d661aa0ae94fe5348721a318c5c1294
alex man <alexman@usc.edu>
2017-03-28 20:56:00 -0700

    added new search functions

--- project2/app/Http/Controllers/PagesController.php
+++ project2/app/Http/Controllers/PagesController.php
@@ -13,16 +13,49 @@ class PagesController extends Controller {
         return view('welcome');
     }
 
-    public function getSearchTerm($search_term)  {
+    /*
+    * function used for Welcome page
+    * returns json data of search result
+    */
+    public function getSearchTerm($search_term, $num_pages)  {
         $term = trim($search_term);
-        $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?querytext=$term";
+        $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?querytext=$term&hc=$num_pages";
         $xml = simplexml_load_file($url, 'SimpleXMLElement', 
             LIBXML_NOCDATA);
         $json = json_encode($xml);
         $array = json_decode($json, TRUE);
 
-        return view('cloud')->with('json_object', $array);
+        return view('welcome');
+    }
 
+    /*
+    * function used to get author 
+    * returns json data of author
+    */
+    public function getAuthor($author, $num_pages) {
+        $author = trim($author);
+        $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?au=$author&hc=$num_pages";
+        $xml = simplexml_load_file($url, 'SimpleXMLElement',
+            LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $author_json = json_decode($json, TRUE);
+
+        return view('welcome');
+    }
+
+    /*
+    * function used to get papers from same conference
+    * returns json data of other papers
+    */
+    public function getConferenceObject($publication_number) {
+        $pub_number = trim($publication_number);
+        $url = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?pn=$pub_number&hc=10";
+        $xml = simplexml_load_file($url, 'SimpleXMLElement',
+            LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $conference_json = json_decode($json, TRUE);
+
+        return view('welcome');
     }
 
     public function goToCloudPage($researcher_name)
